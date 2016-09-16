836c8d2aec5dc840cce859c128a123f689dcff01
fahimraza <fahimraza101@gmail.com>
2016-09-16 15:01:41 +0500

    Delete chit.js

--- chit.js
+++
@@ -1,7 +0,0 @@
-  ( function() {
-    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
-    var unit = {"calltype":"async[2]","publisher":"fahimraza101","width":300,"height":250,"sid":"Chitika Default"};
-    var placement_id = window.CHITIKA.units.length;
-    window.CHITIKA.units.push(unit);
-    document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-}());
