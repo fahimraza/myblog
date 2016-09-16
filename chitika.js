2776e9a20aeb4560f7b5a31a8e1fe86705027a32
fahimraza <fahimraza101@gmail.com>
2016-09-16 15:02:16 +0500

    Create chitika.js

---
+++ chitika.js
@@ -0,0 +1,7 @@
+( function() {
+    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
+    var unit = {"calltype":"async[2]","publisher":"fahimraza101","width":728,"height":90,"sid":"Chitika Default","color_button":"transparent","color_button_text":"086bff"};
+    var placement_id = window.CHITIKA.units.length;
+    window.CHITIKA.units.push(unit);
+    document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+}());
