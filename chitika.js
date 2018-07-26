203c74eb22a6e8239dae98199700e15e5fcc8b90
fahimraza <fahimraza101@gmail.com>
2018-07-26 09:39:38 -0700

    Delete chitika.js

--- chitika.js
+++
@@ -1,7 +0,0 @@
-( function() {
-    if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
-    var unit = {"calltype":"async[2]","publisher":"fahimraza101","width":728,"height":90,"sid":"Chitika Default","color_button":"transparent","color_button_text":"086bff"};
-    var placement_id = window.CHITIKA.units.length;
-    window.CHITIKA.units.push(unit);
-    document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-}());
